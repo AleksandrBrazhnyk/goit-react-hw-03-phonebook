b1b7a3d988700e7d2c50cdaba90e57547e18c6d7
AleksandrBrazhnyk <sanchomus@ukr.net>
2021-07-31 20:48:40 +0300

    hw3 task1

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import style from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={style.form} onSubmit={this.handleSubmit}>
+        <label className={style.label}>
+          Name
+          <input
+            className={style.input}
+            type="text"
+            autoComplete="off"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов."
+            onChange={this.handleChange}
+            required
+            placeholder="Enter name (Ivanov Ivan)"
+          />
+        </label>
+        <label className={style.label}>
+          Phone Number
+          <input
+            className={style.input}
+            type="tel"
+            autoComplete="off"
+            name="number"
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять из цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            onChange={this.handleChange}
+            required
+            placeholder="Enter numbe (111-11-11)"
+          />
+        </label>
+        <button className={style.btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
